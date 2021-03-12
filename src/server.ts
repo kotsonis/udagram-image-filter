1876b0ff9e807d9448b0d00afc8ea855f11e649d
Stefanos Kotsonis <stefankotsonis@gmail.com>
2021-03-12 21:28:48 +0100

    feat: Check image_url
    Implemented checking if image_url is well formed and if it exists.
    Used node-fetch with request for HEAD to do this, which saves time and
    does not retrieve the body of the url

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import { Router, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { url } from 'inspector';
+import fetch from 'node-fetch'; 
+
+// import { config } from 'dotenv';
+
+
+// add for checking if provided url exists
 
 (async () => {
 
@@ -12,7 +20,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
+  
+  // redirect route to our API
+  
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
@@ -30,7 +40,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get('/filteredimage', async(req: Request, res:Response) => {
+    let { image_url } = req.query;
+    console.log(`got a request to process image at url:${image_url}`);
+    // const exists = await urlExists(url_string);
+    
+    try {
+      const response = await fetch(image_url, {method: 'HEAD'});
+      if (response.ok) { // got answer 200~300
+        let files = filterImageFromURL(image_url)
+                    .then((file_list) => {
+                      console.log(`got back the following ${file_list}`)
+                      res.status(200).send({message: `thank you for doing business`})
+                    })
+                    .catch((error)=> {
+                      res.status(400).send({ message: `Something went wrong with processing the image at ${image_url}` });
+                    });
+        
+      } else { // response was note from 200 till 300
+        console.log(`got response ${response.status}, ${response.statusText}`)
+        res.status(400).send({ message: `Image url not ok. Got ${response.status}, ${response.statusText}` });
+      }
+    } catch (error) {
+      return res.status(400).send({ message: `Error: Malformed image_url: ${image_url}` });
+    }
+
+      
+    // console.log(`result of urlExists: ${exists}`);
+    
+});
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
